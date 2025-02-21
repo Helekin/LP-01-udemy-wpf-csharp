d696aecf6b1d4d546023a0cfb46c9efe875ed7c0
Helekin <clsjos2294@gmail.com>
2025-02-20 19:24:21 -0600

    first commit

---
+++ wpf-02-contacts/wpf-02-contacts/NewContactWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using SQLite;
+using wpf_02_contacts.Classes;
+
+namespace wpf_02_contacts
+{
+    /// <summary>
+    /// Lógica de interacción para NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text,
+            };
+
+
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
+
+            Close();
+        }
+    }
+}
